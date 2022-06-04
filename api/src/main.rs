c8e6c07d8fe84314c80b89c49414f752e0912569
Tomáš Diblík <dibla.tomas@post.cz>
2022-06-04 18:43:27 +0200

    [Feature] Add redis graph and sucessfully connect to it

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,17 +1,42 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, post, App, HttpResponse, HttpServer, Responder};
+use redis_graph::*;
 
 #[get("/")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
 
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
+#[post("/moto_gp/create")]
+async fn moto_gp_post() -> impl Responder {
+    let client = redis::Client::open("redis://127.0.0.1/").unwrap();
+    let mut con = client.get_connection().unwrap();
+
+    let _: GraphResultSet = con
+        .graph_query(
+            "my_graph",
+            "CREATE (:Rider {name:'Valentino Rossi'})-[:rides]->(:Team {name:'Yamaha'})",
+        )
+        .unwrap();
+
+    HttpResponse::Ok().body("Since you're seeing this, everything went well")
 }
 
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
+#[get("/moto_gp/get")]
+async fn moto_gp_get() -> impl Responder {
+    let client = redis::Client::open("redis://127.0.0.1/").unwrap();
+    let mut con = client.get_async_connection().await.unwrap();
+
+    let results: GraphResultSet = con
+        .graph_ro_query(
+            "my_graph",
+            "MATCH (rider:Rider)-[:rides]->(:Team {name:'Yamaha'}) RETURN rider",
+        )
+        .await
+        .unwrap();
+
+    println!("{:?}", results);
+
+    HttpResponse::Ok().body("Results are in console")
 }
 
 #[actix_web::main]
@@ -19,8 +44,8 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
+            .service(moto_gp_get)
+            .service(moto_gp_post)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
