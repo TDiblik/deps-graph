2311ef32b6fdb35cc0a857a89427ea41694c01a1
Tomáš Diblík <dibla.tomas@post.cz>
2022-07-17 00:12:26 +0200

    [Feature] Setup swagger

--- api/api/Program.cs
+++ api/api/Program.cs
@@ -1,31 +1,49 @@
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+
 namespace api
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
+            WebApplicationBuilder? builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
             builder.Services.AddControllers();
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(options => {
+                options.SwaggerDoc("v1", new OpenApiInfo {
+                    Version = "v1",
+                    Title = "Deps graph",
+                    Description = "Get graph of your dependencies!",
+                    TermsOfService = new Uri("https://github.com/TDiblik/deps-graph/blob/master/LICENSE"),
+                    Contact = new OpenApiContact {
+                        Name = "Tomáš Diblík's contact info",
+                        Email = "dibla.tomas@post.cz",
+                        Url = new Uri("https://tomasdiblik.cz/")
+                    },
+                    License = new OpenApiLicense {
+                        Name = "License",
+                        Url = new Uri("https://github.com/TDiblik/deps-graph/blob/master/LICENSE")
+                    }
+                });
 
-            var app = builder.Build();
+                string? xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+            });
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+            WebApplication? app = builder.Build();
+
+            // Since this is open source, it's ok to use swagger in production.
+            app.UseSwagger();
+            app.UseSwaggerUI();
 
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
-
 
             app.MapControllers();
 
