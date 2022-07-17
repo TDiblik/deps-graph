d7c1bfb296c055905e23a6e9bc1bc14188db4010
Tomáš Diblík <dibla.tomas@post.cz>
2022-07-18 00:14:04 +0200

    [Chore] Update makefile

--- api/api/Controllers/WeatherForecastController.cs
+++ api/api/Controllers/WeatherForecastController.cs
@@ -18,16 +18,17 @@ namespace api.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
-        }
+        /// <summary>
+        /// Gets all WeatherForecastModel.
+        /// </summary>
+        /// <returns>List of all WeatherForecastModel</returns>
+        /// <response code="200">Returns all items</response>
+        [HttpGet()]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IEnumerable<WeatherForecast> Get() => Enumerable.Range(1, 5).Select(index => new WeatherForecast {
+            Date = DateTime.Now.AddDays(index),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        }).ToArray();
     }
 }
