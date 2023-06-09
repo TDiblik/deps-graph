15e10e85d68660fe729f77cc2dcea953c8dd9d3e
Tomáš Diblík <dibla.tomas@post.cz>
2023-06-09 12:42:56 +0200

    [Feature] Cache traversal results

--- api/src/routes/cargo_router.rs
+++ api/src/routes/cargo_router.rs
@@ -8,7 +8,7 @@ use axum::{
     routing::get,
     Json, Router,
 };
-use redis::aio::Connection;
+use redis::{aio::Connection, AsyncCommands};
 use redis_graph::AsyncGraphCommands;
 use serde::Deserialize;
 use serde_json::{json, Value};
@@ -27,7 +27,7 @@ impl CargoRouter {
 
 #[derive(Deserialize)]
 struct TraverseVersionQueryOptions {
-    root_features: Option<Vec<String>>,
+    root_features: Option<String>,
     root_include_default_features: Option<bool>,
 
     include_normal_dependencies: Option<bool>,
@@ -42,7 +42,39 @@ async fn traverse_version(
     #[cfg(debug_assertions)]
     let time_to_traverse = std::time::Instant::now();
 
+    let root_features_raw = query.root_features.unwrap_or("".to_owned());
+    let root_features = root_features_raw
+        .split(',')
+        .map(|s| s.to_string())
+        .collect();
+    let root_include_default_features = query.root_include_default_features.unwrap_or(true);
+    let include_normal_dependencies = query.include_normal_dependencies.unwrap_or(true);
+    let include_build_dependencies = query.include_build_dependencies.unwrap_or(false);
+    let include_dev_dependencies = query.include_dev_dependencies.unwrap_or(false);
+    let redis_cache_traversal_key = format!(
+        "{}-{}-{}-{}-{}",
+        id,
+        root_features_raw,
+        include_normal_dependencies,
+        include_build_dependencies,
+        include_dev_dependencies
+    );
+
     let mut redis_conn: Connection = app_state.get_redis_conn().await?;
+    let cached_result: Option<String> = redis_conn.get(redis_cache_traversal_key.clone()).await?;
+    if cached_result.is_some() {
+        // Parsing takes shit tone of time, fix in future.
+        let parsed_cached_result: serde_json::Value =
+            serde_json::from_str(cached_result.unwrap().as_str())?;
+
+        #[cfg(debug_assertions)]
+        println!(
+            "Time it took to find in cache and return {}: {:.2?}",
+            id,
+            time_to_traverse.elapsed()
+        );
+        return Ok(Json(parsed_cached_result));
+    }
 
     let root_node_req = redis_conn
         .graph_ro_query(
@@ -55,13 +87,17 @@ async fn traverse_version(
     let answ = traverse_tree(
         &mut redis_conn,
         root_node,
-        query.root_features.unwrap_or(vec![]),
-        query.root_include_default_features.unwrap_or(true),
-        query.include_normal_dependencies.unwrap_or(true),
-        query.include_build_dependencies.unwrap_or(false),
-        query.include_dev_dependencies.unwrap_or(false),
+        root_features,
+        root_include_default_features,
+        include_normal_dependencies,
+        include_build_dependencies,
+        include_dev_dependencies,
     )
     .await?;
+    let json_answ = json!(answ);
+    redis_conn
+        .set(redis_cache_traversal_key, json_answ.to_string())
+        .await?;
 
     #[cfg(debug_assertions)]
     println!(
@@ -69,6 +105,5 @@ async fn traverse_version(
         id,
         time_to_traverse.elapsed()
     );
-
-    Ok(Json(json!(answ)))
+    Ok(Json(json_answ))
 }
